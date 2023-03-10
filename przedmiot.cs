1f880bfa0ee1f3fd797105f153d2a5dcc26969f4
Marcin-256850 <62241302+Marcin-256850@users.noreply.github.com>
2023-03-10 10:38:27 +0100

    Add files via upload

---
+++ przedmiot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nina_Marcin
+{
+    public class item
+    {
+        public int id { get; set; }
+        public int waga { get; set; }
+        public int value { get; set; }
+
+        public void wyswietl()
+        {
+            Console.WriteLine(id,waga,value);
+        }
+    }
+}
